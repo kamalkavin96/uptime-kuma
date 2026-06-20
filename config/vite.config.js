ce46818b73f0f761f8e11b3e998dd58122e04a7b
kamalkavin96 <kamalkavin68@gmail.com>
2026-06-20 14:14:24 +0530

    feat: update server configuration in Vite for improved host handling

--- config/vite.config.js
+++ config/vite.config.js
@@ -12,7 +12,9 @@ const viteCompressionFilter = /\.(js|mjs|json|css|html|svg)$/i;
 export default defineConfig({
     base: process.env.UPTIME_KUMA_BASE_PATH || "",
     server: {
+        host: "0.0.0.0",
         port: 3000,
+        allowedHosts: true,
     },
     define: {
         FRONTEND_VERSION: JSON.stringify(process.env.npm_package_version),
